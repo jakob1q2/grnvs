391cc389ded1f2a3a678c093b75e86e1846d374a
Jakob <jakob.taube1q2@gmail.com>
2020-07-05 16:20:44 +0200

    structure for handling received messages done

--- assignment3/src/Assignment3.java
+++ assignment3/src/Assignment3.java
@@ -17,6 +17,16 @@ public class Assignment3 {
      * To get your own IP address use the getIPv6 function.
      * This one is also documented in GRNVS_RAW.java
      */
+
+    private static final byte versionIpv6 = (byte) 0x60; //version IPv6 (traffic class set 0)
+    private static final byte icmpLength = (byte) 8; //length of ICMPv6 message in byte
+    private static final byte icmpNH = (byte) 0x3a; //Next Header ICMPv6
+    private static final byte icmpEchoRequ = (byte) 128; //ICMP type Echo Request
+    private static final byte icmpEchoRep = (byte) 129; //ICMP type Echo Reply
+    private static final byte icmpTimeEx = (byte) 3; //ICMP type Time Exceeded
+    private static final byte icmpDestUnreach = (byte) 1; //ICMP type Destination unreachable
+    private static final byte icmpID = (byte) 0xab; // ICMP Identifier in Byte 44-45, we modify only 44
+
     public static void run(GRNVS_RAW sock, String dst, int timeout,
                            int attempts, int hopLimit) {
         byte[] buffer = new byte[1514];
@@ -49,19 +59,11 @@ public class Assignment3 {
         boolean dstReached = false;
 
         while (!dstReached && hops <= hopLimit) {
+            Timeout to = new Timeout(timeout);
+            System.out.print(hops);
             for (int attempt = 1; attempt <= attempts; ++attempt) {
 
-                buffer[0] = (byte) 0x60; //version IPv6 (traffic class set 0)
-                buffer[4] = (byte) 8; //length of ICMPv6 message in byte
-                buffer[6] = (byte) 0x3a; //Next Header ICMPv6
-                buffer[7] = (byte) hops;
-                System.arraycopy(srcIp, 0, buffer, 8, 16);
-                System.arraycopy(dst, 0, buffer, 24, 16);
-
-                buffer[40] = (byte) 128; //ICMP type Echo Request
-                buffer[41] = (byte) 0x00; // ICMP Code
-                buffer[44] = (byte) 0xab; // ICMP Identifier in Byte 44-45
-
+                putPerHopStaticIPContent(buffer, srcIp, dstIp, hops);
 
                 bb.putChar(sequenceNumber++);
                 byte[] sequNum = bb.array();
@@ -80,6 +82,23 @@ public class Assignment3 {
                     sock.hexdump(buffer, length);
                     System.exit(1);
                 }
+
+                boolean done = false;
+                while (!done) {
+                    ret = sock.read(buffer, to);
+
+                    if (0 > ret) {
+                        System.err.printf("failed to read from socket: %d\n", ret);
+                        sock.hexdump(buffer, length);
+                        System.exit(1);
+                    }
+                    if (to.getTimeout() == 0) {
+                        System.out.print("  *");
+                        done = true;
+                    } else {
+                        done = checkMessage(buffer, done);
+                    }
+                }
             }
 
         }
@@ -87,6 +106,56 @@ public class Assignment3 {
 
     }
 
+    private static void putPerHopStaticIPContent(byte[] buffer, byte[] srcIp, byte[] dstIp, int hops) {
+        buffer[0] = versionIpv6;
+        buffer[4] = icmpLength;
+        buffer[6] = icmpNH;
+        buffer[7] = (byte) hops;
+        System.arraycopy(srcIp, 0, buffer, 8, 16);
+        System.arraycopy(dstIp, 0, buffer, 24, 16);
+
+        buffer[40] = icmpEchoRequ;
+        buffer[41] = (byte) 0x00; // ICMP Code
+        buffer[44] = icmpID;
+    }
+
+    private static boolean checkMessage(byte[] buffer, boolean done) {
+        int pos = 6;
+        int skip = 34;
+        while (buffer[pos] == (byte) 0x00 || buffer[pos] == (byte) 0x2b || buffer[pos] == (byte) 0x3c) {
+            pos += skip;
+            skip = 8 + 8 * (int) buffer[pos + 1];
+        }
+        if (buffer[pos] == icmpNH) {
+            pos += skip;
+            switch (buffer[pos]) {
+                case icmpTimeEx:
+                    handleIcmpTimeEx(buffer, pos, done);
+                    break;
+                case icmpDestUnreach:
+                    handleIcmpDestUnreach(buffer, pos, done);
+                    break;
+                case icmpEchoRep:
+                    handleIcmpEchoRep(buffer, pos, done);
+                    break;
+                default:
+            }
+        }
+        return done;
+    }
+
+    private static boolean handleIcmpTimeEx(byte[] buffer, int pos, boolean done) {
+        System.out.println();
+        return done;
+    }
+
+    private static boolean handleIcmpDestUnreach(byte[] buffer, int pos, boolean done) {
+        return done;
+    }
+
+    private static boolean handleIcmpEchoRep(byte[] buffer, int pos, boolean done) {
+        return done;
+    }
 
     public static void main(String[] argv) {
         Arguments args = new Arguments(argv);
